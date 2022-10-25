351052baddeacd5a473168ffb270cb4e862fd126
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2022-10-25 19:14:52 -0300

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Relógio Digital
+
+## ⚒️ Technologias
+- HTML
+- CSS
+- JavaScript
+- GIT & GitHub
+
+[Clique aqui para visualizar o projeto](https://vitorbelluzzo.github.io/relogio_digital/)
+
+## 📪 Contato
+
+[vitorbelluzzo@hotmail.com](mailto:vitorbelluzzo@hotmail.com)
---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let DateToday = new Date();
+    let hr = DateToday.getHours();
+    let min = DateToday.getMinutes();
+    let seg = DateToday.getSeconds();
+
+    if(hr < 10) hr = '0' + hr;
+    if(min < 10) min = '0' + min;
+    if(seg < 10) seg = '0' + seg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+    
+    
+})
